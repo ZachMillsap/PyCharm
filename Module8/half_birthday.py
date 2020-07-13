06450fb3b113cafc9f8d297526b5d65d6187f714
Zach Millsap <zach1simillsap@gmail.com>
2020-07-12 23:46:48 -0500

    Module 8: Topic 4:
    DateTime

---
+++ Module8/half_birthday.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+
+
+def half_birthday(date):
+    half_birthday_date = date + timedelta(days=180)
+    return half_birthday_date
+
+
+birthday = datetime(2020,2,3)
+print(half_birthday(birthday))
