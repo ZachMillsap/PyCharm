33370e0c9c20a2d0d51df3fe6a4643da9d747838
Zach Millsap <zach1simillsap@gmail.com>
2020-07-12 22:26:57 -0500

    Module 8: Topic 1: Dictionaries
    V1:Tests Failed

---
+++ Module8/more_fun_with_collections/Dictionaries.py
@@ -0,0 +1,7 @@
+d = {'A':90, 'B':80, 'C':70, 'D': 60, 'F':0}
+print (d)
+
+if "A" in d:
+    del d["A"]
+print(d)
+'A' in d
---
+++ Module8/test_more_fun_with_collections/test_dict_membership.py
@@ -0,0 +1,18 @@
+import unittest
+def in_dict(dict,value):
+    pass
+
+
+
+if __name__ == "__main__":
+
+   test_dict = {"A" : 1, "B" : 2, "C" : 3}
+
+   Test1 = in_dict(test_dict,1)
+
+   print("The Result for Calling function with test_dict, 1 is :", Test1)
+
+   Test2 = in_dict(test_dict,0)
+
+   print("The Result for Calling function with test_dict, 0 is :", Test2)
+
