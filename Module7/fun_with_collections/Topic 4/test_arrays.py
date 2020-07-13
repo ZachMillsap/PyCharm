13b1842fd0f63d5d8e4b9c94dc6aef499a4808fd
Zach Millsap <zach1simillsap@gmail.com>
2020-07-12 21:27:33 -0500

    Module 7: Topic 4 ARRAYS

--- Module7/fun_with_collections/Topic 4/test_arrays.py
+++ Module7/fun_with_collections/Topic 4/test_arrays.py
@@ -1,2 +1,14 @@
+import unittest
+def search_array(array_list): # will return the index of the object in the list or a -1 if the item is not found
+    x = int(input(print("What index would you like to see?")))
+    print(array_list.index(x))
+
+class TestSearchArray(unittest.TestCase):
+
+    def test_search_array(self):
+        self.assertEqual(search_array(1))
+        self.assertNotEqual(search_array(1))
 
 
+if __name__ == '__main__':
+    unittest.main()
