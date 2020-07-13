d971cae9212dffff8bd3c737e3277299882127d1
Zach Millsap <zach1simillsap@gmail.com>
2020-07-12 20:27:29 -0500

    Module 7: Topic 4 ARRAYS

--- Module7/fun_with_collections/Topic 4/test_arrays.py
+++ Module7/fun_with_collections/Topic 4/test_arrays.py
@@ -1,17 +1,2 @@
-import array as arr
 
-def sort_array(array_list):
-    array_list.reverse()
-    return array_list
-
-def search_array(array_list): # will return the index of the object in the list or a -1 if the item is not found
-    x = int(input(print("What index would you like to see?")))
-    print(array_list.index(x))
-
-
-if __name__ == '__main__':
-    array_list = arr.array('d',[1,2,3,4,5])
-    search_array(array_list)
-    sort_array(array_list)
-    print(array_list)   #Returned because I needed to show that the list was reversed
 
